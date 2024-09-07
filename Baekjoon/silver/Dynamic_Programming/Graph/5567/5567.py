bd870e722cbbc5b48eb61a01a4fe5119968421b4
jjjuni <0210leejun@gmail.com>
2024-09-08 00:22:06 +0900

    5567 결혼식

---
+++ Baekjoon/silver/Dynamic_Programming/Graph/5567/5567.py
@@ -0,0 +1,19 @@
+n = int(input())
+m = int(input())
+
+graph = [[] for _ in range(n + 1)]
+inv = [n + 1]
+
+for _ in range(m):
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    graph[a].append(b)
+    graph[b].append(a)
+
+friend = graph[1]
+
+for i in graph[1]:
+    inv[i] = 1
+
+print(inv)
