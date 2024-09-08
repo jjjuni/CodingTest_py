f71996d913663821e94e0e4e16aa0036eb99fbe3
jjjuni <0210leejun@gmail.com>
2024-09-08 17:42:05 +0900

    11725 트리의 부모 찾기

--- Baekjoon/silver/Tree/11725/11725.py
+++ Baekjoon/silver/Tree/11725/11725.py
@@ -1,8 +1,8 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**6)                # 파이썬 기본 재귀 한계 100회 -> 늘려줌
 
 
-def Search(n, node):                        # 트리(인접리스트)를 탐색하며 부모 노드 정보(Parent[]) 저장
+def Search(n, node):                        # 인접리스트(node[])를 탐색하며 부모 노드 정보(Parent[]) 저장
     for i in range(len(node[n])):
         if node_F[node[n][i]] != 1:
             Parent[node[n][i]] = n
@@ -17,7 +17,7 @@ node_F = [0]*(N+1)
 node_F[1] = 1
 Parent = [0]*(N+1)
 
-for _ in range(N-1):
+for _ in range(N-1):                        # 입력 받은 정점 정보를 통해 인접리스트(node[]) 생성
     a, b = map(int, input().split())
                    
     node[b].append(a)
