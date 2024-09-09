81234bdb196da40497944c0a142ae06430090028
이준희 <0210leejun@gmail.com>
2024-09-09 19:57:34 +0900

    24511 queuestack ing...

---
+++ Baekjoon/silver/Queue,Stack/24511/24511.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+
+Q = deque = deque()
+
+A = []
+
+N = int(sys.stdin.readline())
+
+A = sys.stdin.readline().split()
+
+B = sys.stdin.readline().split()
+
+
+M = int(sys.stdin.readline())
+
+C = sys.stdin.readline().split()
