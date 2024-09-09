263bdd311b4a5c2d41a71b67925040691a582064
jjjuni <0210leejun@gmail.com>
2024-09-10 00:50:17 +0900

    24511 queuestack 시간초과...

--- Baekjoon/silver/Queue,Stack/24511/24511.py
+++ Baekjoon/silver/Queue,Stack/24511/24511.py
@@ -1,24 +1,28 @@
 import sys
 from collections import deque
 
-queuestack = []
+str = ""
 
 N = int(sys.stdin.readline())
-
 A = sys.stdin.readline().split()
-
 B = sys.stdin.readline().split()
 
-for i in range(N):
-    if A[i] == 0:
-        Q = deque()
-        queuestack.append(deque)
-    elif A[i] == 1:
-        stack = []
-        queuestack.append(stack)
+queuestack = deque()
 
+for i in range(N-1, -1, -1):
+    if A[i] == '0':                     # 스택은 push 한 값 그대로 pop 하기 배제하고 작성
+        queuestack.append(B[i])         # 남은 큐들을 이어붙이면 또 다른 큐가 됨
 
 M = int(sys.stdin.readline())
-
 C = sys.stdin.readline().split()
 
+for i in range(M):
+    queuestack.append(C[i])
+    tmp = queuestack.popleft()
+
+    if i != 0:
+        str += (" " + tmp)
+    else:
+        str = tmp
+
+print(str)
