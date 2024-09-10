2cff9a8c932002ecf63c279f4a18e7715cd2e947
jjjuni <0210leejun@gmail.com>
2024-09-10 21:25:05 +0900

    5567 결혼식

--- Baekjoon/silver/Graph/5567/5567.py
+++ Baekjoon/silver/Graph/5567/5567.py
@@ -1,8 +1,18 @@
+def invite(num, index):                 # index : 현재 재귀 중첩
+    if index > 2:
+        return 0
+    else:
+        for i in graph[num]:
+            inv[num] = 1
+            invite(i, index+1)
+
 n = int(input())
 m = int(input())
 
+result = 0
+
 graph = [[] for _ in range(n + 1)]
-inv = [n + 1]
+inv = [0]*(n+1)
 
 
 for _ in range(m):
@@ -14,8 +24,10 @@ for _ in range(m):
 
 friend = graph[1]
 
+invite(1, 0)
 
-for i in graph[1]:
-    inv[i] = 1
+for i in inv[2:]:
+    if i == 1:
+        result += 1
 
-print(inv)
+print(result)
