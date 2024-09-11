c28aab261494caba8b8f6bb99abfbe84cb2e883e
이준희 <0210leejun@gmail.com>
2024-09-11 18:03:04 +0900

    2217 거스름돈

--- Baekjoon/silver/greedy/2217/2217.py
+++ Baekjoon/silver/greedy/2217/2217.py
@@ -9,8 +9,8 @@ for i in range(N):
 
 rope.sort(reverse=True)
 
-for i in range(N):
-    if max < (i + 1) * rope[i]:
+for i in range(N):                  # 1개의 로프로 들 수 있는 무게부터 시작
+    if max < (i + 1) * rope[i]:     # 여러개의 로프로 들었을 때 더 많은 무게를 들 수 있다면 max 에 저장
         max = (i + 1) * rope[i]
     
 print(max)
