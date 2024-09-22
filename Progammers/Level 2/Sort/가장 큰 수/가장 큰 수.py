2d076ae204a4dc5b40e56e23d284f1e68fdf5087
jjjuni <0210leejun@gmail.com>
2024-09-23 00:46:35 +0900

    가장 큰 수

--- Progammers/Level 2/Sort/가장 큰 수/가장 큰 수.py
+++ Progammers/Level 2/Sort/가장 큰 수/가장 큰 수.py
@@ -3,7 +3,7 @@ def solution(numbers):
     for i in range(len(numbers)):
         numbers[i] = str(numbers[i])
 
-    numbers.sort(reverse=True, key=lambda x: (x[0], x[1] if len(x) > 1 else x[1%len(x)], x[2] if len(x) > 2 else x[2%len(x)], x[2] if len(x) > 3 else x[3%len(x)]))
+    numbers.sort(reverse=True, key=lambda x: (x[0], x[1%len(x)], x[2%len(x)], x[3%len(x)]))
 
     for i in range(len(numbers)):
         result += numbers[i]
