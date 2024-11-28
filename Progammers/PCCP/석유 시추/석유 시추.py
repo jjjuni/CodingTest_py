655fcc51532603ea2448ad70ce9d37e0e6db877d
jjjuni <0210leejun@gmail.com>
2024-11-28 20:38:15 +0900

    석유 시추 (시간 초과)

--- Progammers/PCCP/석유 시추/석유 시추.py
+++ Progammers/PCCP/석유 시추/석유 시추.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(100000)
+
 def solution(land):
     depth = len(land)
     width = len(land[0])
@@ -5,11 +8,13 @@ def solution(land):
     answer = [0] * width
 
     for i in range(width):
-        landTmp = land
+        landTmp = [arr[:] for arr in land]
         for j in range(depth):
             answer[i] += getOil(j, i, landTmp, None)
         
-    return answer
+    answer.sort()
+
+    return answer[-1]
 
 
 def getOil(y, x, land, prev):
@@ -19,7 +24,7 @@ def getOil(y, x, land, prev):
         oil += 1
         if (prev != 'left' and x != 0):
             oil += getOil(y, x-1, land, 'right')
-        if (prev != 'right' and x + 1 < len(land)):
+        if (prev != 'right' and x + 1 < len(land[0])):
             oil += getOil(y, x+1, land, 'left')
         if (prev != 'up' and y != 0):
             oil += getOil(y-1, x, land, 'down')
@@ -31,3 +36,5 @@ def getOil(y, x, land, prev):
     
 
 print(solution([[0, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0], [1, 1, 0, 0, 0, 1, 1, 0], [1, 1, 1, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 1, 1]]))
+
+#https://school.programmers.co.kr/learn/courses/19344/lessons/242259
