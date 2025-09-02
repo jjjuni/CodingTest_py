6c5346d662736825e2c948a30147fcc588647dbc
jjjuni <0210leejun@gmail.com>
2025-09-02 14:38:26 +0900

    크레인 인형뽑기 게임

---
+++ Progammers/Level 1/크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,19 @@
+def solution(board, moves):
+    answer = 0
+    stack = []
+    for m in moves:
+        for i in range(len(board)):
+            if board[i][m-1] != 0:
+                stack.append(board[i][m-1])
+                board[i][m-1] = 0
+                if len(stack) > 1 and stack[-1] == stack[-2]:
+                    stack.pop()
+                    stack.pop()
+                    answer += 2
+                break
+    
+    return answer
+
+board=[[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves=[1,5,3,5,1,2,1,4]
+print(solution(board, moves))
